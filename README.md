d44f4910fdd22db2d644c2a9e15ac1d8e21bb6a5
MohammadAl-khatib <alk20489@gmail.com>
2021-09-13 23:35:37 +0300

    localhost api

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # city-explorer-api
+**Mohammad Alkhatib**
+**Version: 1.0.0**
+## Overview
+a self created API used to provide City-Explorer app with data needed
+## Architecture
+basic JS and JSON files
+## Credit and Collaborations
+Feras Nawafleh, Mohammad Al-Khatib
---
+++ server.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+require("dotenv").config();
+const weather = require("./data/weather.json");
+const PORT = process.env.PORT;
+
+app.get("/weather", (req, res) => {
+  let lat = Number(req.query.lat);
+  let lon = Number(req.query.lon);
+  let result = [];
+  for (let i = 0; i < weather.length; i++) {
+    if (lat === Number(weather[i].lat) && lon === Number(weather[i].lon)) {
+      for (let j = 0; j < weather[i].data.length; j++) {
+        result[j] = {
+          date: weather[i].data[j].valid_date,
+          description: weather[i].data[j].weather.description,
+        };
+      }
+      res.status(200).json(result);
+    } 
+    else{
+      res.status(400).send("Please enter Amman,Paris, or Seatle");
+    }
+  }
+});
+
+app.listen(PORT, () => {});
